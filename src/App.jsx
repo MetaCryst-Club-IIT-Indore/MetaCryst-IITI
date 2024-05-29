ad6d47d653cfa432b4d1dc27f07ed8e5bbf1b8f6
Dikshit Suthar <suthardikshit57@gmail.com>
2024-05-29 18:40:26 +0530

    loader is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Pages from './Pages/pages'
+import React, { useState, useEffect } from 'react';
+import Pages from './Pages/pages';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
+import Loader from './components/loader/loader';
 
 function App() {
-  
+  const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <>
-    <Pages></Pages>
-      </>
-  )
+  useEffect(() => {
+    
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); 
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) return <Loader />;
+  else return <Pages />;
 }
 
-export default App
+export default App;
