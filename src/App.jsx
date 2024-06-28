457d34ca3a549cd9888a36aba57586c2bf81080e
Dikshit Suthar <suthardikshit57@gmail.com>
2024-06-28 13:58:04 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Loader from './components/loader/loader';
 
+
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
