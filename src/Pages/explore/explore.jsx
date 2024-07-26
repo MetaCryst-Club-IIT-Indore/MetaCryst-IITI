41a769e4b815d5115378b4196d71b2bfe90b03f2
Dhruv Jain <123241810+dhr-jain@users.noreply.github.com>
2024-07-26 17:48:49 +0530

    Update explore.jsx

--- src/Pages/explore/explore.jsx
+++ src/Pages/explore/explore.jsx
@@ -13,13 +13,13 @@ import { Link } from "react-router-dom";
 
 const data = [
   {
-    name: "Steel",
+    name: "Domains in Materials Science",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores enim quia harum blanditiis nam officiis sed voluptates cumque quos, similique veniam error iure veritatis omnis mollitia id minus distinctio eveniet sequi. Error, reiciendis? Nesciunt dolorem, eum quaerat consequuntur delectus aut alias ad soluta similique ducimus, hic in libero neque. Mollitia explicabo recusandae vel corporis unde debitis ea pariatur deserunt iste omnis labore, officiis fugiat sequi temporibus! Ex maxime nesciunt tempora tempore fugit, impedit excepturi ipsum aperiam, quibusdam sed rerum non harum corporis ad. Dicta, labore dolore quisquam adipisci officia autem numquam eligendi dolores doloremque? Quod sit harum commodi cupiditate molestias.",
     link: "https://www.google.com",
   },
   {
-    name: "Steel",
+    name: "GATE Examination",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores enim quia harum blanditiis nam officiis sed voluptates cumque quos, similique veniam error iure veritatis omnis mollitia id minus distinctio eveniet sequi. Error, reiciendis? Nesciunt dolorem, eum quaerat consequuntur delectus aut alias ad soluta similique ducimus, hic in libero neque. Mollitia explicabo recusandae vel corporis unde debitis ea pariatur deserunt iste omnis labore, officiis fugiat sequi temporibus! Ex maxime nesciunt tempora tempore fugit, impedit excepturi ipsum aperiam, quibusdam sed rerum non harum corporis ad. Dicta, labore dolore quisquam adipisci officia autem numquam eligendi dolores doloremque? Quod sit harum commodi cupiditate molestias.",
     link: "https://www.google.com",
