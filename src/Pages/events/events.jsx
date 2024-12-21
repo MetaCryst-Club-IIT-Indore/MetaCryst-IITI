455659d395e0db602dd8e2b9e13d24147cb522e4
Dhruv Jain <jain04dhruv@gmail.com>
2024-12-21 11:30:28 +0530

    images added

--- src/Pages/events/events.jsx
+++ src/Pages/events/events.jsx
@@ -5,27 +5,38 @@ import ScrollToTop from "../../components/scrolltop/scrolltop";
 import styles from "./events.module.css";
 import ECard from "./EventCard";
 import img from "../../assets/design.png";
+import Ferralytics from "../../assets/Ferralytics.png";
+import interact from "../../assets/hero/interact1.jpg";
+import mbq from "../../assets/mbq25.png";
+import te from "../../assets/hero/techexpo1.jpg";
 
 const data = [
   {
     name: "MetaBizz Quiz'25",
     description:"The Annual Quiz Event conducted by the club in which multiple rounds of quizzing takes place. Materials science and metallurgical knowledge is tested in the most fun and exciting way possible. Be a part of this quizzing event to prove your metal-mettle",
     link: "",
-    imgurl: "",
+    imgurl: mbq,
   },
   {
     name: "Ferralytics Challenge'24",
     description:
       "On 20th October 2024, The Metacryst Club organized a Machine Learning challenge aimed at predicting silicon concentration in iron ore using operational data. This initiative helped engineers optimize processes by eliminating the need to wait for lab results, providing participants with an opportunity to solve a real-world, industry-relevant problem.",
     link: "",
-    imgurl: img,
+    imgurl: Ferralytics,
   },
   {
     name: "Independence Day Tech-Expo 2024",
     description:
       "A Tech-Expo was organised by the Science and Technology Council on the Independence day (15th August 2024). All the clubs exhibited their ongoing projects and showcased the talents of their tech teams. Metacryst club actively participated in the expo showing 3 of its ongoing projects related to CMT Welding, Phase field Simulations etc.",
     link: "https://www.google.com",
-    imgurl: img,
+    imgurl: te,
+  },
+  {
+    name: "Alumni Connect 2024",
+    description:
+      "The MetaCryst Club, in collaboration with the MEMS Department, organized a farewell and a special interactive session with the 4th-year passing-out batch of MEMS on April 19th (Friday). The event provided a unique opportunity for students, especially first-year participants, to interact with seniors. The seniors shared their insights, addressed queries, and offered guidance to help students in their academic and professional journeys.",
+    link: "https://www.google.com",
+    imgurl: interact,
   },
   {
     name: "IITISoc 2024",
@@ -34,13 +45,7 @@ const data = [
     link: "https://www.google.com",
     imgurl: img,
   },
-  {
-    name: "Alumni Connect 2024",
-    description:
-      "The MetaCryst Club, in collaboration with the MEMS Department, organized a farewell and a special interactive session with the 4th-year passing-out batch of MEMS on April 19th (Friday). The event provided a unique opportunity for students, especially first-year participants, to interact with seniors. The seniors shared their insights, addressed queries, and offered guidance to help students in their academic and professional journeys.",
-    link: "https://www.google.com",
-    imgurl: img,
-  },
+  
 ];
 
 function Events() {
