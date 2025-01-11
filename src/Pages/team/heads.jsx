7b759c8ce87c4b14fee0a192014a1d1dde669f48
Abhishek A P <abhi2004iiti@gmail.com>
2025-01-11 16:16:40 +0530

    minor changes

--- src/Pages/team/heads.jsx
+++ src/Pages/team/heads.jsx
@@ -1,16 +1,8 @@
-import React, { useState } from 'react';
-import '../../components/teamcard/teamcard.css';
+import React, { useState } from "react";
+import "../../components/teamcard/teamcard.css";
 // import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
-import Teamcard from '../../components/teamcard/teamcard'; 
-import {
-
-  Abhishek,
- 
-  Avdhoot,
-  Dhruv,
-  Lokery,
-  Mrunal,
-} from "../../assets/team";
+import Teamcard from "../../components/teamcard/teamcard";
+import { Abhishek, Avdhoot, Dhruv, Lokery, Mrunal } from "../../assets/team";
 
 function Head() {
   const [committee] = useState({
@@ -19,9 +11,9 @@ function Head() {
         name: "Dhruv Jain",
         img: Dhruv,
         designation: "Overall Head",
-        ig: "",
+        ig: "/",
         lin: "https://in.linkedin.com/in/dhr-jain",
-        git: "https://github.com/dhr-jain"
+        git: "https://github.com/dhr-jain",
       },
       {
         name: "Avdhoot Kasture",
@@ -29,36 +21,33 @@ function Head() {
         designation: "Treasurer",
         ig: "https://www.instagram.com/avdhoot_kasture20_04/",
         lin: "https://in.linkedin.com/in/avdhoot-kasture",
-        git: "https://github.com/kal-el95"
+        git: "https://github.com/kal-el95",
       },
       {
-    
         name: "Abhishek AP",
         img: Abhishek,
         designation: "Projects Head",
         ig: "https://www.instagram.com/abhiap2004/?hl=en",
         lin: "https://www.linkedin.com/in/abhishek-a-p-66104425b/",
-        git: "https://github.com/Abhiap2004"
+        git: "https://github.com/Abhiap2004",
       },
       {
-    
         name: "Lokery Kiranmayi",
         img: Lokery,
         designation: "Outreach Head",
-        ig: "",
+        ig: "/",
         lin: "https://www.linkedin.com/in/kiranmayi-lokery-34402725b",
-        git: ""
+        git: "/",
       },
       {
-    
         name: "Mrunal Nandpure",
         img: Mrunal,
         designation: "Events Head",
         ig: "https://www.instagram.com/mrunal_nandpure/",
         lin: "https://www.linkedin.com/in/mrunal-nandpure-5b90a025b",
-        git: ""
+        git: "/",
       },
-    ]
+    ],
   });
 
   return (
